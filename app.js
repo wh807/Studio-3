11a9b177342449ab43eea3007248b16d3b8087bc
“wh807” <hao_wen@brown.edu>
2022-09-27 17:47:02 -0400

    dfd

--- app.js
+++ app.js
@@ -26,12 +26,24 @@ const updatePage = async () => {
 
   // Make API request and get an array of fruit objects
   const fruitsArray = await apiRequest();
-  // console.log(fruitsArray);
+  console.log(fruitsArray);
 
   // TODO: Use either `map` and/or `filter` to extract some data from the array of fruit objects
   // For example, find "name of all fruits whose sugar > 15",
 
+  filtered = fruitsArray.filter(fruit => fruit.id > 50)
+  names = filtered.map((item) => {return item.name})
+
   // TODO: Create a new HTML element to display your data
+
+
+  let list = document.getElementById("myList");
+  
+  names.forEach((item)=>{
+    let li = document.createElement("li");
+    li.innerText = item;
+    list.appendChild(li);
+  })
 
   // TODO: Append your new element to the page
 
